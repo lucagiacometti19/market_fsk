a7f1750ef65a0047482a55a2db5f0ce01bcd37a2
Luca Giacometti <luca.giacometti@studenti.unitn.it>
2022-12-04 23:46:17 +0100

    New lib version & added new tests

--- src/tests.rs
+++ src/tests.rs
@@ -18,6 +18,8 @@ mod test {
         //test new_random
         market_test::test_new_random::<MarketType>();
         market_test::new_random_should_not_exceeed_starting_capital::<MarketType>();
+        //test new with quantities
+        market_test::should_initialize_with_right_quantity::<MarketType>();
         //test get_buy_price
         market_test::test_get_buy_price_insufficient_qty_error::<MarketType>();
         market_test::test_get_buy_price_non_positive_error::<MarketType>();
@@ -25,6 +27,7 @@ mod test {
         //test get_sell_price
         market_test::test_get_sell_price_non_positive_error::<MarketType>();
         market_test::test_get_sell_price_success::<MarketType>();
+        //test deadlock prevention
         market_test::test_deadlock_prevention::<MarketType>();
         //sell tests
         market_test::test_sell_err_expired_token::<MarketType>();
@@ -32,9 +35,22 @@ mod test {
         market_test::test_sell_err_unrecognized_token::<MarketType>();
         market_test::test_sell_err_wrong_good_kind::<MarketType>();
         market_test::test_sell_success::<MarketType>();
-        //test_price_change
+        //buy tests
+        market_test::test_buy_good_kind_not_default::<MarketType>();
+        //market_test::test_buy_insufficient_good_quantity::<MarketType>();
+        market_test::test_buy_unrecognized_token::<MarketType>();
+        market_test::test_buy_success::<MarketType>();
+        //test price_change
         market_test::price_changes_waiting::<MarketType>();
         market_test::test_price_change_after_buy::<MarketType>();
         market_test::test_price_change_after_sell::<MarketType>();
+        //test get budget
+        market_test::test_get_budget::<MarketType>();
+        //test get buy price
+        market_test::test_get_buy_price_insufficient_qty_error::<MarketType>();
+        market_test::test_get_buy_price_non_positive_error::<MarketType>();
+        market_test::test_get_buy_price_success::<MarketType>();
+        //get goods test
+        market_test::test_get_goods::<MarketType>();
     }
 }
